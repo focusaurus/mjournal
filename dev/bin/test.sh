34166ae78da5e16c63936d820a42f994f95c2d2f
Peter Lyons <pete@peterlyons.com>
2013-10-16 21:51:16 -0600

    use node-dev for go start

--- dev/bin/test.sh
+++ dev/bin/test.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-runTestsSqlite() {
-  DB_PATH="var/mj-test.sqlite"
-  > "${DB_PATH}"
-  cat app/ddl/users.sql | sqlite3 "${DB_PATH}"
-  NODE_ENV=test MJ_DB_URL="sqlite3://${DB_PATH}" mocha ${ARGS}
-}
-
 runTestsPostgres() {
   psql --username=mjournal --dbname=mjournal-test --file=app/db/postgres/destroyData.sql
   NODE_ENV=test MJ_DB_URL="postgres://mjournal@localhost/mjournal-test" mocha ${ARGS}
@@ -14,6 +7,6 @@ runTestsPostgres() {
 cd $(dirname "${0}")/../..
 ARGS=""
 if [ "${1}" == "--debug" ]; then
-  ARGS="--timeout 0 --debug-brk=9091"
+  ARGS="--timeout 0 --debug-brk=9093"
 fi
 runTestsPostgres
