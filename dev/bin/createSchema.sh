28220f28186006bdb28c4bceecef3cf0d25d7c37
Peter Lyons <pete@peterlyons.com>
2014-02-09 16:28:17 -0700

    reorganize bin scripts and move toward google style guide

--- dev/bin/createSchema.sh
+++
@@ -1,1 +0,0 @@
-runsql.sh
--- dev/bin/runsql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#This script is designed to have many symlinks with different names
-# the .sh symlinks should map to a corresponding .sql file
-DIR=$(dirname "${0}")/../../app/db/postgres
-BASE=$(basename "${0}" | cut -d . -f 1)
-for DB in mjournal mjournal-test
-do
-	psql --username=mjournal --file="${DIR}/${BASE}.sql" "${DB}"
-done
