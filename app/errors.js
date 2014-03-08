58d3ba984f639dc424dd6a0520c1bd4e8e6103c1
Peter Lyons <pete@peterlyons.com>
2014-03-07 18:04:12 -0700

    oh snap have all HTTP error codes now. Thanks Wikipedia.

--- app/errors.js
+++ app/errors.js
@@ -1,6 +1,17 @@
 var util = require("util");
+//code data obtained from
+//https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
 
-function makeError(name, message, code) {
+function messageToName(message) {
+  return message.replace(/[-' ]/g, "");
+}
+require("./codes").forEach(function (struct) {
+  var code = struct[0];
+  var message = struct[1];
+  exports[messageToName(message)] = makeError(message, code);
+});
+
+function makeError(message, code) {
   function constructor(message, code) {
     if (!(this instanceof constructor)) {
       return new constructor(message, code);
@@ -10,15 +21,12 @@ function makeError(name, message, code) {
     Error.call(this, message);
   }
   util.inherits(constructor, Error);
-  constructor.name = name;
+  constructor.name = messageToName(message);
   constructor.prototype.message = message;
   constructor.prototype.code = code;
   return constructor;
 }
 
-module.exports = {
-  ServerError: makeError("ServerError", "internal server error", 503),
-  ClientError: makeError("ClientError", "precondition failed", 412),
-  Unauthorized: makeError("Unauthorized", "unauthorized", 401),
-  Conflict: makeError("Conflict", "conflict", 409)
-}
+//A handful of convenient aliases
+exports.ClientError = exports.PreconditionFailed;
+exports.ServerError = exports.InternalServerError;
--- app/tests/errors.js
+++ app/tests/errors.js
@@ -15,7 +15,7 @@ describe("errors.ClientError", function () {
   it("should be have default code and message", function() {
     var error = errors.ClientError();
     expect(error).to.have.property("code", 412);
-    expect(error).to.have.property("message", "precondition failed");
+    expect(error).to.have.property("message", "Precondition Failed");
   });
   it("should be allow specifying a particular message", function() {
     var error = errors.ClientError("foo");
@@ -41,7 +41,7 @@ describe("errors.Conflict", function () {
   it("should be have default code and message", function() {
     var error = errors.Conflict();
     expect(error).to.have.property("code", 409);
-    expect(error).to.have.property("message", "conflict");
+    expect(error).to.have.property("message", "Conflict");
   });
   it("should be allow specifying a particular message", function() {
     var error = errors.Conflict("foo");
@@ -53,35 +53,35 @@ describe("errors.Conflict", function () {
   });
 });
 
-describe("errors.ServerError", function () {
+describe("errors.InternalServerError", function () {
   it("should be callable as a constructor with new", function() {
-    var error = new errors.ServerError();
+    var error = new errors.InternalServerError();
     expect(error).to.be.instanceof(Error);
-    expect(error).to.be.instanceof(errors.ServerError);
+    expect(error).to.be.instanceof(errors.InternalServerError);
   });
   it("should be callable as a function without new", function() {
-    var error = errors.ServerError();
+    var error = errors.InternalServerError();
     expect(error).to.be.instanceof(Error);
-    expect(error).to.be.instanceof(errors.ServerError);
+    expect(error).to.be.instanceof(errors.InternalServerError);
   });
   it("should be have default code and message", function() {
-    var error = errors.ServerError();
-    expect(error).to.have.property("code", 503);
-    expect(error).to.have.property("message", "internal server error");
+    var error = errors.InternalServerError();
+    expect(error).to.have.property("code", 500);
+    expect(error).to.have.property("message", "Internal Server Error");
   });
   it("should be allow specifying a particular message", function() {
-    var error = errors.ServerError("foo");
+    var error = errors.InternalServerError("foo");
     expect(error).to.have.property("message", "foo");
   });
   it("should be allow specifying a particular code", function() {
-    var error = errors.ServerError("foo", 442);
+    var error = errors.InternalServerError("foo", 442);
     expect(error).to.have.property("code", 442);
   });
 });
 
 describe("errors module", function () {
-  it("should have expected constructors", function() {
-    ["Unauthorized"].forEach(function (constructor) {
+  it("should have expected constructors and handle", function() {
+    ["Unauthorized", "Imateapot", "RequestURITooLong", "PreconditionFailed"].forEach(function (constructor) {
       expect(errors).to.have.property(constructor);
       expect(errors[constructor]).to.be.a("function");
     });
