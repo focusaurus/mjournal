f58650cc8822e04faf8167e289a27afa781741ce
Peter Lyons <pete@peterlyons.com>
2014-04-27 18:36:43 -0600

    more styling including fonts

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #mjournal
 
 Minimalist journal aiming to be one journal for all of your technical projects. Geared toward sparse and clean UI and categorization through labels.
+
+(grid background image stolen from http://bulletjournal.com)
--- app/index.mocha.js
+++ app/index.mocha.js
@@ -1,5 +1,14 @@
 var testUtils = require("app/testUtils");
 describe("app/index site-wide routes", function() {
+  ["/grid.gif", "/tag.png"].forEach(function(url) {
+    it("GET " + url + " should 200/image", function(done) {
+      testUtils.get(url)
+        .expect(200)
+        .expect("Content-Type", /image/)
+        .end(done);
+    });
+  });
+
   it("GET /mjournal.css should send CSS", function(done) {
     this.timeout(2000); //Sorry. FS IO
     testUtils
