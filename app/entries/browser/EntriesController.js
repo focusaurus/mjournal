c5f13b535e71e2ee128d87185140d2946e53aa13
Peter Lyons <pete@peterlyons.com>
2015-01-22 09:21:19 -0700

    require 2 clicks to delete an entry

--- app/entries/browser/EntriesController.js
+++ app/entries/browser/EntriesController.js
@@ -3,7 +3,7 @@ var _ = require("lodash");
 var ENTER = 13;
 var PAGE_SIZE = 50;
 
-function EntriesController($scope, $q, $location, Entries) {
+function EntriesController($scope, $q, $location, $timeout, Entries) {
   $scope.get = function get() {
     var params = $location.search();
     Entries.get(params, function(entries) {
@@ -75,6 +75,14 @@ function EntriesController($scope, $q, $location, Entries) {
   };
 
   $scope.delete = function del(entry) {
+    if (!entry.confirmingDelete) {
+      entry.confirmingDelete = true;
+      entry.confirmPromise = $timeout(function () {
+        delete entry.confirmingDelete;
+      }, 3000);
+      return;
+    }
+    $timeout.cancel(entry.confirmPromise);
     Entries.delete({id: entry.id}, function () {
       $scope.entries = _.without($scope.entries, entry);
     });
