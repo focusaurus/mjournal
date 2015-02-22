760342bc8893af85bbfc430541546188270cd852
Peter Lyons <pete@peterlyons.com>
2015-02-22 12:36:32 -0700

    scroll to top component

--- app/entries/browser/index.js
+++ app/entries/browser/index.js
@@ -2,8 +2,9 @@ module.exports = "mjournal.entries";
 require("angular");
 require("angular-resource");
 require("ng-tags-input");
+require("ng-scroll-to");
 
-angular.module(module.exports, ["ngResource", "ngTagsInput"])
+angular.module(module.exports, ["ngResource", "ngTagsInput", "ngScrollTo"])
   .config(function entriesConfig($locationProvider) {
     $locationProvider.html5Mode(true);
   })
