891adcc05f4cd623b0c94d217c3030965bef30ea
Peter Lyons <pete@peterlyons.com>
2015-05-07 22:08:21 -0600

    busybox/boot2docker support

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,8 +9,9 @@ template() {
   ./bin/render-template.js "$1" | ssh "${docker}" tee "$2" > /dev/null
 }
 
-echo "Starting containers on ${docker}"
+echo "copying scripts to ${docker}"
 template "deploy/backup-db.mustache" "/tmp/backup-${app_name}-db"
 template "deploy/nginx.mustache" "/tmp/nginx_${app_name}"
 template "deploy/setup_docker.sh.mustache" "/tmp/setup_docker_${app_name}.sh"
-# ssh -t ${docker} sudo /bin/sh "/tmp/setup_docker_${app_name}.sh"
+echo "running docker setup script on ${docker}"
+ssh -t ${docker} sudo /bin/sh "/tmp/setup_docker_${app_name}.sh"
