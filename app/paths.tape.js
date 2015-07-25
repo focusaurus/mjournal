e292cd145be5e472a981b4169c0cb4e7e9c991c4
Peter Lyons <pete@peterlyons.com>
2015-07-25 10:52:21 -0600

    mocha->tape migration

---
+++ app/paths.tape.js
@@ -0,0 +1,11 @@
+var test = require('tape')
+var paths = require('./paths')
+
+test('app/paths should have the expected exported strings', function (assert) {
+  assert.equal(typeof paths.app, 'string')
+  assert.equal(typeof paths.browser, 'string')
+  assert.equal(typeof paths.build, 'string')
+  assert.equal(typeof paths.thirdParty, 'string')
+  assert.equal(typeof paths.wwwroot, 'string')
+  assert.end()
+})
