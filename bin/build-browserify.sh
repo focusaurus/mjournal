0319764e28d3c81b0cde08bb755b2ceb6643973b
Peter Lyons <pete@peterlyons.com>
2016-06-12 18:11:10 -0600

    Use latest bash strict mode boilerplate

--- bin/build-browserify.sh
+++ bin/build-browserify.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # build browser js
+
 # This does not source ./lib/strict-mode.sh for convenience within docker
+
+# Please Use Google Shell Style: https://google.github.io/styleguide/shell.xml
+# Start unofficial bash strict mode boilerplate
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -o errexit    # always exit on error
 set -o errtrace   # trap errors in functions as well
 set -o pipefail   # don't ignore exit codes when piping output
@@ -8,7 +13,10 @@ set -o posix      # more strict failures in subshells
 # set -x          # enable debugging
 
 IFS="$(printf "\n\t")"
+# End unofficial bash strict mode boilerplate
+
 cd "$(dirname "$0")/.."
 
 PATH=$(npm bin):$PATH
-browserify -e ./app/browser.js | uglifyjs --compress --screw-ie8 - > wwwroot/mjournal.js
+browserify -e ./app/browser.js \
+  | uglifyjs --compress --screw-ie8 - > wwwroot/mjournal.js
