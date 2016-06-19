a2b4f99196fb95b88d9836ce08bd2c33e81c2587
Peter Lyons <pete@peterlyons.com>
2016-06-19 13:23:21 -0600

    Enable browserify debug source maps again

--- bin/build-browserify.sh
+++ bin/build-browserify.sh
@@ -4,7 +4,7 @@
 # This does not source ./lib/strict-mode.sh for convenience within docker
 
 # Please Use Google Shell Style: https://google.github.io/styleguide/shell.xml
-# Start unofficial bash strict mode boilerplate
+# ---- Start unofficial bash strict mode boilerplate
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -o errexit    # always exit on error
 set -o errtrace   # trap errors in functions as well
@@ -13,10 +13,15 @@ set -o posix      # more strict failures in subshells
 # set -x          # enable debugging
 
 IFS="$(printf "\n\t")"
-# End unofficial bash strict mode boilerplate
+# ---- End unofficial bash strict mode boilerplate
 
 cd "$(dirname "$0")/.."
 
 PATH=$(npm bin):$PATH
-browserify -e ./app/browser.js \
-  | uglifyjs --compress --screw-ie8 - > wwwroot/mjournal.js
+if [[ "$(config3 MJ_DEBUG_BROWSERIFY)" == "true" ]]; then
+  browserify --debug -e ./app/browser.js > wwwroot/mjournal.js
+else
+  browserify -e ./app/browser.js \
+    | uglifyjs --compress --screw-ie8 - \
+    > wwwroot/mjournal.js
+fi
