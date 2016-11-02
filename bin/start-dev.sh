1ded9265d07a9b22e81204f6349ad9f8b5188087
Peter Lyons <pete@peterlyons.com>
2016-11-02 11:08:37 -0600

    chore: disable inspector for now
    port conflicts are a pain for development

--- bin/start-dev.sh
+++ bin/start-dev.sh
@@ -4,4 +4,5 @@
 cd "$(dirname "$0")/.." || exit 10
 source ./bin/lib/strict-mode.sh
 PATH=$(npm bin):$PATH
-node-dev --debug=9091 --inspect . | tee -a logs/mjournal.log
+# node-dev --debug=9091 --inspect . | tee -a logs/mjournal.log
+node-dev . | tee -a logs/mjournal.log
