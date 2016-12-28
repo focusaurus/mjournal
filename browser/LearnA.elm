4044b6cf67ecc7dbcd246d1514e9312b11001513
Peter Lyons <pete@peterlyons.com>
2016-12-28 09:59:47 -0700

    LearnA with ValueKeyCode basics working

--- browser/LearnA.elm
+++ browser/LearnA.elm
@@ -3,10 +3,11 @@ module LearnA exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on)
+import Json.Decode as JD
 
 
 type Msg
-    = Msg1
+    = ValueKeyCode String Int
 
 
 type alias Model =
@@ -14,11 +15,25 @@ type alias Model =
     }
 
 
+onDown : (String -> Int -> Msg) -> Attribute Msg
+onDown tagger =
+    on "keydown"
+        (JD.map2
+            (\s i -> tagger s i)
+            (JD.at [ "target", "value" ] JD.string)
+            (JD.field "keyCode" JD.int)
+        )
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update message model =
     case message of
-        Msg1 ->
-            ( model, Cmd.none )
+        ValueKeyCode value keyCode ->
+            let
+                _ =
+                    Debug.log "ValueKeyCode" value ++ ": " ++ toString keyCode
+            in
+                ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -30,7 +45,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Learn Elm A" ]
-        , input [] []
+        , input [ onDown ValueKeyCode ] []
         ]
 
 
