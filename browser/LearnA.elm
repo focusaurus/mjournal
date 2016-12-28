7b20ea45b675fd4d71268ec5de124cace27b95d6
Peter Lyons <pete@peterlyons.com>
2016-12-28 09:48:58 -0700

    Basic LearnA lab

--- browser/LearnA.elm
+++ browser/LearnA.elm
@@ -1,128 +1,49 @@
-import Html exposing (Html, a, button, code, div, h1, li, text, ul)
-import Html.Attributes exposing (href)
-import Html.Events exposing (onClick)
-import Http
-import Navigation
-import UrlParser as Url exposing ((</>), (<?>), s, int, stringParam, top)
+module LearnA exposing (main)
 
-
-
-main =
-  Navigation.program UrlChange
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
-
-
-
--- MODEL
-
-
-type alias Model =
-  { history : List (Maybe Route)
-  }
-
-
-init : Navigation.Location -> ( Model, Cmd Msg )
-init location =
-  ( Model [Url.parsePath route location]
-  , Cmd.none
-  )
-
-
-
--- URL PARSING
-
-
-type Route
-  = Home
-  | BlogList (Maybe String)
-  | BlogPost Int
-
-
-route : Url.Parser (Route -> a) a
-route =
-  Url.oneOf
-    [ Url.map Home top
-    , Url.map BlogList (s "blog" <?> stringParam "search")
-    , Url.map BlogPost (s "blog" </> int)
-    ]
-
-
-
--- UPDATE
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on)
 
 
 type Msg
-  = NewUrl String
-  | UrlChange Navigation.Location
+    = Msg1
+
+
+type alias Model =
+    { tags : List String
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  case msg of
-    NewUrl url ->
-      ( model
-      , Navigation.newUrl url
-      )
-
-    UrlChange location ->
-      ( { model | history = Url.parsePath route location :: model.history }
-      , Cmd.none
-      )
-
-
-
--- SUBSCRIPTIONS
+update message model =
+    case message of
+        Msg1 ->
+            ( model, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
-
-
-
--- VIEW
+    Sub.none
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "Links" ]
-    , ul [] (List.map viewLink [ "/", "/blog/", "/blog/42", "/blog/37", "/blog/?search=cats" ])
-    , h1 [] [ text "History" ]
-    , ul [] (List.map viewRoute model.history)
-    ]
+    div []
+        [ h1 [] [ text "Learn Elm A" ]
+        , input [] []
+        ]
 
 
-viewLink : String -> Html Msg
-viewLink url =
-  li [] [ button [ onClick (NewUrl url) ] [ text url ] ]
+init : ( Model, Cmd Msg )
+init =
+    ( Model [], Cmd.none )
 
 
-viewRoute : Maybe Route -> Html msg
-viewRoute maybeRoute =
-  case maybeRoute of
-    Nothing ->
-      li [] [ text "Invalid URL"]
-
-    Just route ->
-      li [] [ code [] [ text (routeToString route) ] ]
-
-
-routeToString : Route -> String
-routeToString route =
-  case route of
-    Home ->
-      "home"
-
-    BlogList Nothing ->
-      "list all blog posts"
-
-    BlogList (Just search) ->
-      "search for " ++ Http.encodeUri search
-
-    BlogPost id ->
-      "show blog " ++ toString id
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
