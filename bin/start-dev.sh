dbb179d0396b6fd7814c96f88121c7b89dbe4c2e
Peter Lyons <pete@peterlyons.com>
2017-01-25 10:25:30 -0700

    Figured out the node-dev piped to json hack

--- bin/start-dev.sh
+++ bin/start-dev.sh
@@ -4,5 +4,13 @@
 cd "$(dirname "$0")/.." || exit 10
 source ./bin/lib/strict-mode.sh
 PATH=$(npm bin):$PATH
-# node-dev --debug=9091 --inspect . | tee -a logs/mjournal.log
-node-dev . | tee -a logs/mjournal.log
+
+# NOTE: node-dev does not work when output is piped, so no tee
+# or json formatting for you here
+# node-dev --debug=9091 --inspect .
+
+# This grep trick below took a long time to figure out
+node-dev . \
+  | grep --line-buffered -v '^\[INFO' \
+  | tee -a logs/mjournal.log \
+  | json -g -a -0 -e 'delete this.v; delete this.hostname;delete this.level; delete this.pid; delete this.name'
